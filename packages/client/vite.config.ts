713389ce51914103f8b73f4e13b21adc5670854f
Aman Varshney <amanvarshney.work@gmail.com>
2025-01-25 22:28:32 +0530

    use tailwindcss's vite plugin

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react()],
+  plugins: [tailwindcss(), TanStackRouterVite({}), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
